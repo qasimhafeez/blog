ee61abfbbeb841d17bc9afc3c5a0e014535ac714
Qasim Hafeez <qasimh556@gmail.com>
2020-09-19 17:47:48 +0400

    added custom posts

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php include "includes/header.php" ?>
-
     <header class="header">
       <!-- Main Navbar-->
       <nav class="navbar navbar-expand-lg">
@@ -49,11 +48,60 @@
           <div class="container">
             <div class="row">
               <!-- post -->
+              <?php
+
+                //This is For Pagination
+
+                $posts_per_page = 6;
+
+                if(isset($_GET['page'])){
+
+                    $page = $_GET['page'];
+
+                }
+                else {
+                    $page = "";
+
+                }
+
+                if ($page == "" || $page == 1) {
+                
+                    $page_1 = 0;
+                
+                } else {
+                    $page_1 = ($page * $posts_per_page) - $posts_per_page;
+                }
+
+
+
+                $find_rows_numbers = "select * from posts";
+                $get_count = mysqli_query($connection, $find_rows_numbers);
+                $count = mysqli_num_rows($get_count);
+                $count = ceil($count / $posts_per_page);
+
+
+                $query = "select * from posts LIMIT $page_1,$posts_per_page";
+                $result = mysqli_query($connection, $query);
+                
+                while($row = mysqli_fetch_assoc($result))
+                {
+                    $post_id = $row['post_id'];
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = substr($row['post_content'], 0, 225);
+                    $post_status = $row['post_status'];
+                    
+                    if($post_status == 'published')
+                    {
+                        
+                ?>
               <div class="post col-xl-6">
                 <div class="post-thumbnail"><a href="post.html"><img src="img/blog-post-1.jpeg" alt="..." class="img-fluid"></a></div>
                 <div class="post-details">
                   <div class="post-meta d-flex justify-content-between">
-                    <div class="date meta-last">20 May | 2016</div>
+                    <div class="date meta-last"><?php echo $post_date; ?></div>
                     <div class="category"><a href="#">Business</a></div>
                   </div><a href="post.html">
                     <h3 class="h4">Alberto Savoia Can Teach You About Interior</h3></a>
@@ -66,69 +114,29 @@
                   </footer>
                 </div>
               </div>
-              <!-- post             -->
-              <div class="post col-xl-6">
-                <div class="post-thumbnail"><a href="post.html"><img src="img/blog-post-2.jpg" alt="..." class="img-fluid"></a></div>
-                <div class="post-details">
-                  <div class="post-meta d-flex justify-content-between">
-                    <div class="date meta-last">20 May | 2016</div>
-                    <div class="category"><a href="#">Business</a></div>
-                  </div><a href="post.html">
-                    <h3 class="h4">Alberto Savoia Can Teach You About Interior</h3></a>
-                  <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                  <div class="post-footer d-flex align-items-center"><a href="#" class="author d-flex align-items-center flex-wrap">
-                      <div class="avatar"><img src="img/avatar-2.jpg" alt="..." class="img-fluid"></div>
-                      <div class="title"><span>John Doe</span></div></a>
-                    <div class="date"><i class="icon-clock"></i> 2 months ago</div>
-                    <div class="comments meta-last"><i class="icon-comment"></i>12</div>
-                  </div>
-                </div>
-              </div>
-              <!-- post             -->
-              <div class="post col-xl-6">
-                <div class="post-thumbnail"><a href="post.html"><img src="img/blog-post-3.jpeg" alt="..." class="img-fluid"></a></div>
-                <div class="post-details">
-                  <div class="post-meta d-flex justify-content-between">
-                    <div class="date meta-last">20 May | 2016</div>
-                    <div class="category"><a href="#">Business</a></div>
-                  </div><a href="post.html">
-                    <h3 class="h4">Alberto Savoia Can Teach You About Interior</h3></a>
-                  <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                  <div class="post-footer d-flex align-items-center"><a href="#" class="author d-flex align-items-center flex-wrap">
-                      <div class="avatar"><img src="img/avatar-3.jpg" alt="..." class="img-fluid"></div>
-                      <div class="title"><span>John Doe</span></div></a>
-                    <div class="date"><i class="icon-clock"></i> 2 months ago</div>
-                    <div class="comments meta-last"><i class="icon-comment"></i>12</div>
-                  </div>
-                </div>
-              </div>
-              <!-- post -->
-              <div class="post col-xl-6">
-                <div class="post-thumbnail"><a href="post.html"><img src="img/blog-post-4.jpeg" alt="..." class="img-fluid"></a></div>
-                <div class="post-details">
-                  <div class="post-meta d-flex justify-content-between">
-                    <div class="date meta-last">20 May | 2016</div>
-                    <div class="category"><a href="#">Business</a></div>
-                  </div><a href="post.html">
-                    <h3 class="h4">Alberto Savoia Can Teach You About Interior</h3></a>
-                  <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                  <div class="post-footer d-flex align-items-center"><a href="#" class="author d-flex align-items-center flex-wrap">
-                      <div class="avatar"><img src="img/avatar-1.jpg" alt="..." class="img-fluid"></div>
-                      <div class="title"><span>John Doe</span></div></a>
-                    <div class="date"><i class="icon-clock"></i> 2 months ago</div>
-                    <div class="comments meta-last"><i class="icon-comment"></i>12</div>
-                  </div>
-                </div>
-              </div>
+              <?php
+                    }
+                }
+                ?>
+              <!-- End row -->
             </div>
             <!-- Pagination -->
             <nav aria-label="Page navigation example">
               <ul class="pagination pagination-template d-flex justify-content-center">
-                <li class="page-item"><a href="#" class="page-link"> <i class="fa fa-angle-left"></i></a></li>
+                <!-- <li class="page-item"><a href="#" class="page-link"> <i class="fa fa-angle-left"></i></a></li>
                 <li class="page-item"><a href="#" class="page-link active">1</a></li>
                 <li class="page-item"><a href="#" class="page-link">2</a></li>
                 <li class="page-item"><a href="#" class="page-link">3</a></li>
-                <li class="page-item"><a href="#" class="page-link"> <i class="fa fa-angle-right"></i></a></li>
+                <li class="page-item"><a href="#" class="page-link"> <i class="fa fa-angle-right"></i></a></li> -->
+                <?php 
+                for ($i=1; $i <= $count; $i++) {
+                    if ($i == $page) {
+                        echo "<li class='page-item'><a href='index.php?page={$i}' class='page-link active'>{$i}</a></li>";     
+                    } else {
+                        echo "<li class='page-item'><a href='#' class='page-link active'>1</a></li>";    
+                    }   
+                }
+                ?>
               </ul>
             </nav>
           </div>
