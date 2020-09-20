71ad0bc56ef2d391ec424a36a4b65c4c916322b8
Qasim Hafeez <qasimh556@gmail.com>
2020-09-20 11:07:53 +0400

    fixed logo

--- includes/logo.php
+++ includes/logo.php
@@ -1,5 +1,5 @@
-<div class="navbar-header d-flex align-items-center justify-content-between" style="margin-top: -25px" >
-<!-- Navbar Brand --><a href="index.html" class="navbar-brand"><img src="images/logo.png" class="img img-fluid" style="max-height: 85px;" alt="logo"></a>
+<div class="navbar-header d-flex align-items-center justify-content-between" style="margin-top: -20px" >
+<!-- Navbar Brand --><a href="index.php" class="navbar-brand"><img src="images/logo.png" class="img img-fluid" style="max-height: 60px; margin-bottom: -25px" alt="logo"></a>
 <!-- Toggle Button-->
 <button type="button" data-toggle="collapse" data-target="#navbarcollapse" aria-controls="navbarcollapse" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><span></span><span></span><span></span></button>
 </div>
