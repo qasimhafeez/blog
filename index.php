9ce5dea9334cfbc0a4e48c06047c1c6e54550e36
Qasim Hafeez <qasimh556@gmail.com>
2020-09-20 11:09:27 +0400

    fixed logo

--- index.php
+++ index.php
@@ -24,11 +24,7 @@
         </div>
         <div class="container">
           <!-- Navbar Brand -->
-          <div class="navbar-header d-flex align-items-center justify-content-between">
-            <!-- Navbar Brand --><a href="index.html" class="navbar-brand">Bootstrap Blog</a>
-            <!-- Toggle Button-->
-            <button type="button" data-toggle="collapse" data-target="#navbarcollapse" aria-controls="navbarcollapse" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><span></span><span></span><span></span></button>
-          </div>
+          <?php include "includes/logo.php" ?>
           <!-- Navbar Menu -->
           <div id="navbarcollapse" class="collapse navbar-collapse">
             <!-- <ul class="navbar-nav ml-auto">
