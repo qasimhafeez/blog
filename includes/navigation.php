10df3427680290d6564b42efd11d9a232cf51a5a
Qasim Hafeez <qasimh556@gmail.com>
2020-09-21 11:31:54 +0400

    changed navigation style

---
+++ includes/navigation.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg    fixed-top">
+      <div class="container" style="margin-bottom: -20px;
+    margin-top: -17px;">
+  <div style="float: left;
+    margin-top: 0px;
+    width: 26%;">
+<a href="https://www.lurninghub.com"><img src="images/LurningHubLogo.jpg"></a>
+
+</div>
+<form name="searchForm" id="searchForm" action="/search.php" class="form-inline my-2 my-lg-0" method="post">
+<span><i class="fa fa-search" aria-hidden="true"></i></span>
+<input class="form-control mr-sm-2 topsearch" type="text" autocomplete="off" name="inputsearch" placeholder="Search" aria-label="Search">
+<div class="search-result" style="display: block;"></div>
+</form>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <ul class="navbar-nav ml-auto">
+<li><a class="nav-link" href="forum.php" style="color: #5059a5; padding-top: 0;"><p style="margin-bottom: 0px;"><i class="fa fa-comments" aria-hidden="true" style="transform: scale(1.5);"></i></p>FORUM</a></li>
+<li class="active"><a class="nav-link" href="blog.php" style="color: #6ad044 !important;padding-top: 0;"><p style="margin-bottom: 0px;"><i class="fa fa-commenting-o" aria-hidden="true" style="transform: scale(1.5);"></i></p>BLOG</a></li>
+<li><a href="login.php"><button type="button" class="btn btn-secondary requestbut" data-toggle="modal" data-target="#loginModal" style="background-color: #515eb9 !important; margin-top:5px">LOGIN</button></a></li>
+</ul>
+  </div>
+</nav>
